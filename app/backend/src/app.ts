6b142fac131dbefc66dd687dca06e97253e37027
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-10-06 18:29:22 -0300

    commiting app file

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import errorMiddleware from './middlewares/error.middleware';
+import loginRouter from './routes/login.router';
 
 class App {
   public app: express.Express;
@@ -22,6 +24,8 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
+    this.app.use('/login', loginRouter);
+    this.app.use(errorMiddleware);
   }
 
   public start(PORT: string | number):void {
